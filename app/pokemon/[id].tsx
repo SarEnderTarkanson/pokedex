78704191a39bdeb4eefc3ea246423c72f14ca57a
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-06-03 23:35:30 +0200

    started the pokemon detail screen

--- app/pokemon/[id].tsx
+++ app/pokemon/[id].tsx
@@ -1,13 +1,56 @@
-import { useLocalSearchParams } from "expo-router";
-import { View, Text } from "react-native";
+import { RootView } from "@/components/RootView";
+import { Row } from "@/components/Row";
+import { ThemedText } from "@/components/ThemedText";
+import { useFetchQuery } from "@/hooks/useFetchQuery";
+import { router, useLocalSearchParams } from "expo-router";
+import { View, Text, StyleSheet, Image, Pressable } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Pokemon() {
-  const params = useLocalSearchParams();
+  const params = useLocalSearchParams() as { id: string };
+  const { data: pokemon } = useFetchQuery("/pokemon/[id]", { id: params.id });
 
   return (
-    <SafeAreaView>
-      <Text>Pokemon {params.id}</Text>
-    </SafeAreaView>
+    <RootView>
+      <View>
+        <Image
+          style={styles.pokeball}
+          source={require("@/assets/images/Pokeball_Big.png")}
+          width={200}
+          height={200}
+        />
+        <Row style={styles.header}>
+          <Pressable onPress={router.back}>
+            <Row gap={8}>
+              <Image
+                source={require("@/assets/images/arrow_back.png")}
+                width={32}
+                height={32}
+              />
+              <ThemedText color="grayWhite" variant="headline">
+                {pokemon?.name}
+              </ThemedText>
+            </Row>
+          </Pressable>
+          <ThemedText color="grayWhite" variant="subtitle2">
+            #{params.id.padStart(3, "0")}
+          </ThemedText>
+        </Row>
+        <Text>Pokemon {params.id}</Text>
+      </View>
+    </RootView>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    margin: 20,
+    justifyContent: "space-between",
+  },
+  pokeball: {
+    opacity: 0.1,
+    position: "absolute",
+    right: 8,
+    top: 8,
+  },
+});
