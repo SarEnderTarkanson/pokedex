5de065c7cfcc14f293bf75e7c15b49a38f62d71f
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-06-08 02:26:17 +0200

    pokemon details screen continued

--- app/pokemon/[id].tsx
+++ app/pokemon/[id].tsx
@@ -1,12 +1,15 @@
+import { Card } from "@/components/Card";
 import { RootView } from "@/components/RootView";
 import { Row } from "@/components/Row";
 import { ThemedText } from "@/components/ThemedText";
 import { Colors } from "@/constants/Colors";
+import { getPokemonArtwork } from "@/functions/pokemon";
 import { useFetchQuery } from "@/hooks/useFetchQuery";
 import { useThemeColors } from "@/hooks/useThemeColors";
 import { router, useLocalSearchParams } from "expo-router";
 import { View, Text, StyleSheet, Image, Pressable } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { PokemonType } from "./PokemonType";
 
 export default function Pokemon() {
   const colors = useThemeColors();
@@ -14,9 +17,10 @@ export default function Pokemon() {
   const { data: pokemon } = useFetchQuery("/pokemon/[id]", { id: params.id });
   const mainType = pokemon?.types?.[0].type.name;
   const colorType = mainType ? Colors.type[mainType] : colors.tint;
+  const types = pokemon?.types ?? [];
 
   return (
-    <RootView style={{backgroundColor: colorType}}>
+    <RootView style={{ backgroundColor: colorType }}>
       <View>
         <Image
           style={styles.pokeball}
@@ -32,7 +36,11 @@ export default function Pokemon() {
                 width={32}
                 height={32}
               />
-              <ThemedText color="grayWhite" variant="headline">
+              <ThemedText
+                color="grayWhite"
+                variant="headline"
+                style={{ textTransform: "capitalize" }}
+              >
                 {pokemon?.name}
               </ThemedText>
             </Row>
@@ -41,6 +49,23 @@ export default function Pokemon() {
             #{params.id.padStart(3, "0")}
           </ThemedText>
         </Row>
+        <View style={styles.body}>
+          <Image
+            style={styles.artwork}
+            source={{
+              uri: getPokemonArtwork(params.id),
+            }}
+            width={200}
+            height={200}
+          />
+          <Card style={styles.card}>
+            <Row style={{ justifyContent: "center" }} gap={16}>
+              {types.map((type) => (
+                <PokemonType name={type.type.name} key={type.type.name} />
+              ))}
+            </Row>
+          </Card>
+        </View>
         <Text>Pokemon {params.id}</Text>
       </View>
     </RootView>
@@ -57,5 +82,18 @@ const styles = StyleSheet.create({
     position: "absolute",
     right: 8,
     top: 8,
+  },
+  artwork: {
+    position: "absolute",
+    top: -140,
+    alignSelf: "center",
+    zIndex: 2,
+  },
+  body: {
+    marginTop: 144,
+  },
+  card: {
+    paddingHorizontal: 20,
+    paddingTop: 60,
   },
 });
--- components/pokemon/PokemonCard.tsx
+++ components/pokemon/PokemonCard.tsx
@@ -3,6 +3,7 @@ import { Card } from "@/components/Card";
 import { ThemedText } from "@/components/ThemedText";
 import { useThemeColors } from "@/hooks/useThemeColors";
 import { Link } from "expo-router";
+import { getPokemonArtwork } from "@/functions/pokemon";
 
 type Props = {
   style?: ViewStyle;
@@ -14,14 +15,17 @@ export function PokemonCard({ style, id, name }: Props) {
   const colors = useThemeColors();
   return (
     <Link href={{ pathname: "/pokemon/[id]", params: { id: id } }} asChild>
-      <Pressable android_ripple={{ color: colors.tint, foreground: true }} style={style}>
+      <Pressable
+        android_ripple={{ color: colors.tint, foreground: true }}
+        style={style}
+      >
         <Card style={[styles.card]}>
           <ThemedText style={styles.id} variant="caption" color="grayMedium">
             #{id.toString().padStart(3, "0")}
           </ThemedText>
           <Image
             source={{
-              uri: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`,
+              uri: getPokemonArtwork(id),
             }}
             width={72}
             height={72}
