e36b2740f3950f23ad28efca77c371936c1cb132
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-06-13 02:48:15 +0200

    continue

--- app/pokemon/[id].tsx
+++ app/pokemon/[id].tsx
@@ -3,21 +3,35 @@ import { RootView } from "@/components/RootView";
 import { Row } from "@/components/Row";
 import { ThemedText } from "@/components/ThemedText";
 import { Colors } from "@/constants/Colors";
-import { getPokemonArtwork } from "@/functions/pokemon";
+import {
+  formatWeight,
+  formatHeight,
+  getPokemonArtwork,
+} from "@/functions/pokemon";
 import { useFetchQuery } from "@/hooks/useFetchQuery";
 import { useThemeColors } from "@/hooks/useThemeColors";
 import { router, useLocalSearchParams } from "expo-router";
 import { View, Text, StyleSheet, Image, Pressable } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { PokemonType } from "./PokemonType";
+import { PokemonSpec } from "@/components/pokemon/PokemonSpec";
+import { PokemonStat } from "@/components/pokemon/PokemonStat";
 
 export default function Pokemon() {
   const colors = useThemeColors();
   const params = useLocalSearchParams() as { id: string };
   const { data: pokemon } = useFetchQuery("/pokemon/[id]", { id: params.id });
+  const { data: species } = useFetchQuery("/pokemon-species/[id]", {
+    id: params.id,
+  });
   const mainType = pokemon?.types?.[0].type.name;
   const colorType = mainType ? Colors.type[mainType] : colors.tint;
   const types = pokemon?.types ?? [];
+  const bio = Array.isArray(species?.flavor_text_entries)
+    ? species.flavor_text_entries
+        .find(({ language }) => language.name === "en")
+        ?.flavor_text.replaceAll("\n", " ")
+    : undefined;
 
   return (
     <RootView style={{ backgroundColor: colorType }}>
@@ -59,11 +73,55 @@ export default function Pokemon() {
             height={200}
           />
           <Card style={styles.card}>
-            <Row style={{ justifyContent: "center" }} gap={16}>
+            <Row gap={16}>
               {types.map((type) => (
                 <PokemonType name={type.type.name} key={type.type.name} />
               ))}
             </Row>
+            <ThemedText variant="subtitle1" style={{ color: colorType }}>
+              About
+            </ThemedText>
+            <Row>
+              <PokemonSpec
+                style={{
+                  borderStyle: "solid",
+                  borderRightWidth: 1,
+                  borderColor: colors.grayLight,
+                }}
+                title={formatWeight(pokemon?.weight)}
+                description="Weight"
+                image={require("@/assets/images/weight.png")}
+              />
+              <PokemonSpec
+                style={{
+                  borderStyle: "solid",
+                  borderRightWidth: 1,
+                  borderColor: colors.grayLight,
+                }}
+                title={formatHeight(pokemon?.height)}
+                description="Size"
+                image={require("@/assets/images/straighten.png")}
+              />
+              <PokemonSpec
+                title={pokemon?.moves
+                  .slice(0, 2)
+                  .map((m) => m.move.name)
+                  .join("\n")}
+                description="Moves"
+              />
+            </Row>
+            <ThemedText>{bio}</ThemedText>
+            <ThemedText variant="subtitle1" style={{ color: colorType }}>
+              Base Stats
+            </ThemedText>
+            <View>
+              <PokemonStat name={"HP"} value={45} color={colorType} />
+              <PokemonStat name={"HP"} value={45} color={colorType} />
+              <PokemonStat name={"HP"} value={45} color={colorType} />
+              <PokemonStat name={"HP"} value={45} color={colorType} />
+              <PokemonStat name={"HP"} value={45} color={colorType} />
+              <PokemonStat name={"HP"} value={45} color={colorType} />
+            </View>
           </Card>
         </View>
         <Text>Pokemon {params.id}</Text>
@@ -95,5 +153,7 @@ const styles = StyleSheet.create({
   card: {
     paddingHorizontal: 20,
     paddingTop: 60,
+    gap: 16,
+    alignItems: "center",
   },
 });
